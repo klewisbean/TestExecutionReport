bd74f1f1004c5dbf2027cb70e8fad1caa8b45326
klewis <klewis@llbean.com>
2016-07-25 14:36:10 -0400

    update

--- TestExecutionReport/src/main/java/com/llbean/automation/extractor/AutomateExportGUI.java
+++ TestExecutionReport/src/main/java/com/llbean/automation/extractor/AutomateExportGUI.java
@@ -31,7 +31,7 @@ public class AutomateExportGUI {
 
     public static void main(String[] args){
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
-
+        System.out.println("user.dir in main: " + System.getProperty("user.dir"));
 
         AutomateExportGUI main = new AutomateExportGUI();
 
