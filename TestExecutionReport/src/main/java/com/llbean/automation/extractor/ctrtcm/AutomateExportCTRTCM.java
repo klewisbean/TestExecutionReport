9edaba0e084c1452acd1fb591a2c85d8b64594bc
klewis <klewis@llbean.com>
2016-08-16 10:41:10 -0400

    changed rel for ctrtcm

--- TestExecutionReport/src/main/java/com/llbean/automation/extractor/ctrtcm/AutomateExportCTRTCM.java
+++ TestExecutionReport/src/main/java/com/llbean/automation/extractor/ctrtcm/AutomateExportCTRTCM.java
@@ -38,7 +38,7 @@ public class AutomateExportCTRTCM {
     final static Logger logger = Logger.getLogger(AutomateExportCTRTCM.class);
 
     public static void main(String[] args){
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\TestExecutionReport\\drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
         logger.error("user.dir in main: " + System.getProperty("user.dir"));
 
         AutomateExportCTRTCM main = new AutomateExportCTRTCM();
